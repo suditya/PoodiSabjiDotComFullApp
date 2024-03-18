8e495246ad556611ceb0be39056d0c954715ab73
suditya <guptasuditya@gmail.com>
2024-03-18 12:29:48 +0530

    destructured folder

--- Poodi-Sabji-dot-com/frontend/src/utility/common.tsx
+++
@@ -1,6 +0,0 @@
-export const BACKEND_DEV_URL = `http://localhost:3000/api`;
-
-export const isNullOrUndefined = (value: any): boolean => {
-  if (value === undefined || value === null) return true;
-  else return false;
-};
