f651a6ef0cd844be9ae0fc1e3b5b158e4a5cad9b
suditya <guptasuditya@gmail.com>
2024-03-18 13:23:28 +0530

    commented code

--- backend/src/data/db.ts
+++ backend/src/data/db.ts
@@ -1,10 +1,10 @@
 import { MongoClient } from "mongodb";
 const DB_NAME = "Poodi-Sabji-dot-com";
-let uri = `mongodb://0.0.0.0:27017/${DB_NAME}`;
-if (process.env.NODE_ENV != "development" || true) {
-  uri =
-    "mongodb+srv://suditya:Suditya%40123@poodisabjidotcom.jjmenhc.mongodb.net/PoodiSabjiDotCom?retryWrites=true&w=majority&appName=PoodiSabjiDotCom";
-}
+// let uri = `mongodb://0.0.0.0:27017/${DB_NAME}`;
+// if (process.env.NODE_ENV != "development" || true) {
+//   uri =
+//     "mongodb+srv://suditya:Suditya%40123@poodisabjidotcom.jjmenhc.mongodb.net/PoodiSabjiDotCom?retryWrites=true&w=majority&appName=PoodiSabjiDotCom";
+// }
 // console.log(uri);
 const client = new MongoClient(
   "mongodb+srv://suditya:Suditya%40123@poodisabjidotcom.jjmenhc.mongodb.net/PoodiSabjiDotCom?retryWrites=true&w=majority&appName=PoodiSabjiDotCom",
